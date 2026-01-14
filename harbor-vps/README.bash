a2ae4874bd32ba771d87476207cc9b7299a853e6
ymlstudy <dvhbb663@gmail.com>
2026-01-14 22:26:12 +0800

    Update README.bash

--- harbor-vps/README.bash
+++ harbor-vps/README.bash
@@ -67,9 +67,10 @@ docker-compose up -d
 三、Harbor 配置代理缓存
 
 在 Harbor 中创建 Proxy Cache 项目，创建 Proxy Cache 类型的项目，按需命名：
-
+h1.jpg
 
 在 Harbor 中创建上面的这几个名字的项目，打开镜像代理，一一对应。
+h2.jpg
 
 四、Kubernetes 配置
 1，配置 K8s 节点信任 Harbor 证书
