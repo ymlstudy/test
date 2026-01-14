acc3fd86005fde4f3b62a8e94676600b4f797774
ymlstudy <dvhbb663@gmail.com>
2026-01-14 22:33:39 +0800

    Update README.bash

--- harbor-vps/README.bash
+++ harbor-vps/README.bash
@@ -67,7 +67,6 @@ docker-compose up -d
 三、Harbor 配置代理缓存
 
 在 Harbor 中创建 Proxy Cache 项目，创建 Proxy Cache 类型的项目，按需命名：
-![Image Example](https://raw.githubusercontent.com/ymlstudy/test/refs/heads/main/harbor-vps/h1.png)
 
 https://raw.githubusercontent.com/ymlstudy/test/refs/heads/main/harbor-vps/h1.png
 
